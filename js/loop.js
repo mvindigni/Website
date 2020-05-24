eb46324bf4c566f5c32314a364eb22af6310dc56
Matt <mrvindigni@wpi.edu>
2020-05-23 22:25:50 -0400

    added more informational labels

--- js/loop.js
+++ js/loop.js
@@ -1,4 +1,6 @@
 var points = 0;
+var machines = 0;
+var machineRate = 5;
 
 function pointClick(number){
     points += number;
@@ -10,25 +12,22 @@ function addPoints(number){
     document.getElementById('points').innerHTML = points + " POINTS";
 }
 
-var machines = 0;
-var machineRate = 5;
-
 function buyMachine(){
     var machineCost = Math.floor(20 * Math.pow(1.5, machines));
     if(points >= machineCost){
         machines += 1;
         points -= machineCost;
-        document.getElementById('machines').innerHTML = machines;
-        document.getElementById('points').innerHTML = points;
-        document.getElementById('pps').innerHTML = (machines*machineRate).toString() + " points per second";
+        document.getElementById('points').innerHTML = points + " POINTS";
+        document.getElementById('machines').innerHTML = machines + " MACHINES";
     };
 
-    var nextCost = Math.floor(20 * Math.pow(1.1, machines));
+    var nextCost = Math.floor(20 * Math.pow(1.5, machines));
     document.getElementById('machineCost').innerHTML = nextCost.toString() + " POINTS";
 }
 
 window.setInterval(function(){
 
     addPoints(machines*machineRate)
+    document.getElementById('pps').innerHTML = Math.floor(machines*machineRate) + " POINTS PER SECOND"
 
 }, 1000);
